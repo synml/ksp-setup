022a1e79c2b532732cc086f8744cc9388c403396
Lulin Pollux <kistssy+dev@gmail.com>
2019-06-23 16:53:07 +0900

    어셈블리 정보 수정
    어셈블리 설명, 버전

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
 [assembly: AssemblyTitle("KSP-Setup")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("KSP 설정")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Labbit Lulin")]
 [assembly: AssemblyProduct("KSP-Setup")]
@@ -51,5 +51,5 @@ using System.Windows;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호를
 // 기본값으로 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
